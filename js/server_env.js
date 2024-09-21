04e162411b0706bd01fa9cef8001153a5b24c507
Zachary Lamb <102142742+zlamb1@users.noreply.github.com>
2024-09-21 03:40:39 -0500

    fixed port issue

--- js/server_env.js
+++ js/server_env.js
@@ -1,8 +1,9 @@
 export default {
     getWebServerPorts: () => {
+        const isProduction = process.env.NODE_ENV === 'production';
         return {
-            http: process.env.HTTP_PORT || 8080,
-            https: process.env.HTTPS_PORT || 8443,
+            http: process.env.HTTP_PORT || (isProduction ? 80 : 8080),
+            https: process.env.HTTPS_PORT || (isProduction ? 443 : 8443),
         }
     },
     getLiveServerPort: () => {
