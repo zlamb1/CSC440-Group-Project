824bbf9e1ddb3c4dc23a10254a4b4873e6780c74
Zachary Lamb <102142742+zlamb1@users.noreply.github.com>
2024-09-23 11:40:23 -0500

    progressive enchancement for dark mode

--- app/utils/prefers-color-scheme.ts
+++ app/utils/prefers-color-scheme.ts
@@ -1,2 +1,5 @@
 export const prefersColorSchemeCookieName = 'client-hint-prefers-color-scheme';
 export const defaultColorScheme = 'system';
+export const defaultFallbackColorScheme = 'dark';
+
+export const colorSchemes = [ 'light', 'dark', 'system' ];
--- app/utils/theme-script.tsx
+++ app/utils/theme-script.tsx
@@ -1,35 +1,81 @@
 import React from "react";
-import {defaultColorScheme, prefersColorSchemeCookieName} from "@/utils/prefers-color-scheme";
+import {
+    colorSchemes,
+    defaultColorScheme,
+    defaultFallbackColorScheme,
+    prefersColorSchemeCookieName
+} from "@/utils/prefers-color-scheme";
 
 export function ThemeClientScript() {
     return (
         <script dangerouslySetInnerHTML={{__html: `
+            function testCookies() {
+                let cookieEnabled = navigator.cookieEnabled;
+                if (cookieEnabled) {
+                    try {
+                        document.cookie = 'cookietest=1';
+                        const ret = document.cookie.indexOf('cookietest=') !== -1;
+                        // delete cookie
+                        document.cookie = 'cookietest=1; expires=Thu, 01-Jan-1970 00:00:01 GMT';
+                        return ret;
+                    } catch (err) {
+                        return false;
+                    }
+                }
+                return cookieEnabled;
+            }
+        
+            function testLocalStorage() {
+                try {
+                    localStorage.setItem('local_storage_enabled', true); 
+                    if (localStorage.getItem('local_storage_enabled') !== 'true') {
+                        return false; 
+                    }
+                    localStorage.removeItem('local_storage_enabled');
+                    return true; 
+                } catch (err) {
+                    return false; 
+                }
+            }
+            
+            const cookieEnabled = testCookies();
+            const storageEnabled = testLocalStorage(); 
+            const mediaQuery = window.matchMedia ? window.matchMedia('(prefers-color-scheme: dark)') : undefined; 
+                
             function getCookie(name) {
-                name = name + "=";
-                const decodedCookies = decodeURIComponent(document.cookie);
-                const cookies = decodedCookies.split(';');
-                for (let cookie of cookies) {
-                    while (cookie.charAt(0) === ' ')
-                        cookie = cookie.substring(1); 
-                    if (cookie.indexOf(name) == 0)
-                        return cookie.substring(name.length, cookie.length); 
+                if (cookieEnabled) {
+                    name = name + "=";
+                    const decodedCookies = decodeURIComponent(document.cookie);
+                    const cookies = decodedCookies.split(';');
+                    for (let cookie of cookies) {
+                        while (cookie.charAt(0) === ' ')
+                            cookie = cookie.substring(1); 
+                        if (cookie.indexOf(name) == 0)
+                            return cookie.substring(name.length, cookie.length); 
+                    }
                 }
                 return null;
             }
             
             function getPreferredColorScheme() {
-                // default to dark
-                if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-                    return 'dark';
+                if (window.matchMedia) {
+                    if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+                        return 'dark'; 
+                    }
+                    if (window.matchMedia('(prefers-color-scheme: light)').matches) {
+                        return 'light'; 
+                    }
                 }
-                return 'light';
+                return '${defaultFallbackColorScheme}';
             }
             
             const chPrefersColorScheme = '${prefersColorSchemeCookieName}'; 
             function setColorSchemeCookie(scheme) {
-                // set expiration date for 10 years in future
-                const expiration = 60 * 60 * 24 * 365 * 10;
-                document.cookie = chPrefersColorScheme + '=' + scheme + ';SameSite=Lax;MaxAge=' + expiration + ';Path=/'; 
+                if (cookieEnabled) {
+                    // set expiration date for 10 years in future
+                    const expiration = 60 * 60 * 24 * 365 * 10;
+                    document.cookie = chPrefersColorScheme + '=' + scheme + ';Same-Site=Lax;Max-Age=' + expiration + ';Path=/'; 
+                }
             }
             
             function getActiveScheme() {
@@ -46,40 +92,83 @@ export function ThemeClientScript() {
                 }
             }
             
-            function updateColorScheme() {
+            function validateColorScheme(scheme) {
+                if (scheme === 'system' && !mediaQuery) return false; 
+                return [${colorSchemes.map(scheme => `'${scheme}'`).join(',')}].includes(scheme); 
+            }
+            
+            function updateColorScheme(value) {
                 if (typeof window !== "undefined") {
-                    const cookie = getCookie(chPrefersColorScheme); 
+                    const cookie = value ?? getCookie(chPrefersColorScheme); 
                     const activeScheme = getActiveScheme(); 
                     const preferredScheme = getPreferredColorScheme(); 
-                    if (cookie) {
-                        if (cookie === 'system') {
-                            if (activeScheme !== preferredScheme) {
-                                updateDOM(preferredScheme); 
+                    if (cookieEnabled) {
+                        if (cookie) {
+                            if (cookie === 'system') {
+                                if (activeScheme !== preferredScheme) {
+                                    updateDOM(preferredScheme); 
+                                }
+                            } else if (activeScheme !== cookie) {
+                                updateDOM(cookie); 
                             }
-                        } else if (activeScheme !== cookie) {
-                            updateDOM(cookie); 
+                        } else if (mediaQuery) {
+                            updateDOM(preferredScheme);
+                            setColorSchemeCookie('${defaultColorScheme}'); 
+                        } else {
+                            updateDOM('${defaultFallbackColorScheme}');
+                            setColorSchemeCookie('${defaultFallbackColorScheme}'); 
+                        }
+                    } else if (storageEnabled) {
+                        // fallback to localStorage
+                        const storageScheme = value ?? localStorage.getItem(chPrefersColorScheme);
+                        if (storageScheme && validateColorScheme(storageScheme)) {
+                            if (storageScheme === 'system') {
+                                updateDOM(preferredScheme); 
+                            } else {
+                                updateDOM(storageScheme); 
+                            }
+                        } else if (mediaQuery) {
+                            updateDOM(preferredScheme);
+                            localStorage.setItem(chPrefersColorScheme, preferredScheme);
+                        } else {
+                            updateDOM('${defaultFallbackColorScheme}');
+                            localStorage.setItem(chPrefersColorScheme, '${defaultFallbackColorScheme}');
                         }
                     } else {
-                        updateDOM(preferredScheme);
-                        setColorSchemeCookie('${defaultColorScheme}'); 
+                        updateDOM('${defaultFallbackColorScheme}'); 
                     }
                 }
             }
             
-            if (window && window.matchMedia) {
-                updateColorScheme(); 
-                window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', e => updateColorScheme());
+            updateColorScheme(); 
+            
+            if (mediaQuery) {
+                mediaQuery.addEventListener('change', e => updateColorScheme());
+                // backward-compatability
+                mediaQuery.addListener(e => updateColorScheme());
             }
             
-            // poll for changes in cookie
-            let cachedCookie = getCookie(chPrefersColorScheme); 
-            setInterval(() => {
-                const cookie = getCookie(chPrefersColorScheme); 
-                if (cachedCookie !== cookie) {
-                    cachedCookie = cookie;
-                    updateColorScheme(cookie); 
-                }
-            }, 1000); 
+            if (cookieEnabled) {
+                // poll for changes in cookie
+                let cachedCookie = getCookie(chPrefersColorScheme); 
+                setInterval(() => {
+                    const cookie = getCookie(chPrefersColorScheme); 
+                    if (cachedCookie !== cookie) {
+                        cachedCookie = cookie;
+                        updateColorScheme(cookie); 
+                    }
+                }, 1000); 
+            } else if (storageEnabled) {
+                // poll for changes in localStorage
+                let cachedValue = localStorage.getItem(chPrefersColorScheme); 
+                setInterval(() => {
+                    const value = localStorage.getItem(chPrefersColorScheme); 
+                    if (cachedValue !== value) {
+                        cachedValue = value;
+                        updateColorScheme(value); 
+                    }
+                }, 1000); 
+            }
         `}} />
     )
 }
