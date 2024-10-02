743b326a45597c7a5543e9896011c79a89721800
Zachary Lamb <102142742+zlamb1@users.noreply.github.com>
2024-10-02 17:11:59 -0500

    updated _index layout

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -16,30 +16,32 @@ export default function Index() {
         return <NotImplemented />
     }
     return (
-        <div className="flex flex-col gap-3 items-center mt-16">
-            <Card className="w-[50%] p-5">
-                <Form navigate={false} className="flex flex-col gap-5 items-center" action="/create-post" method="post">
-                    <span className="font-bold select-none">Create a Post</span>
-                    <Textarea placeholder="Enter your message here." name="content" required/>
-                    <Button type="submit">Upload Post</Button>
-                </Form>
-            </Card>
-            {
-                data?.posts.map((post: any) => {
-                    return (
-                        <Card className="p-3 flex flex-col gap-3 select-none" key={post.id}>
-                            <span>Posted by {post.user_name}</span>
-                            <span>Posted at {post.posted_at}</span>
-                            <span>Post content: {post.content}</span>
-                            {
-                                post.poster_id === data.user.id ?
-                                    (<Form navigate={false} action="/delete-post" method="post"><input name="id" className="invisible" readOnly value={post.id} /><Button className="bg-red-600 hover:bg-red-500">Delete</Button></Form>) :
-                                    null
-                            }
-                        </Card>
-                    );
-                })
-            }
+        <div className="flex justify-center">
+            <div className="flex flex-col gap-3 mt-16 w-[90%] md:w-[75%] xl:w-[50%]">
+                <Card className="p-5">
+                    <Form navigate={false} className="flex flex-col gap-5 items-center" action="/create-post" method="post">
+                        <span className="text-xl font-bold select-none">Create a Post</span>
+                        <Textarea placeholder="Enter your message here." name="content" required/>
+                        <Button type="submit">Upload Post</Button>
+                    </Form>
+                </Card>
+                {
+                    data?.posts.map((post: any) => {
+                        return (
+                            <Card className="p-3 flex flex-col gap-3 select-none w-full" key={post.id}>
+                                <span>Posted by {post.user_name}</span>
+                                <span>Posted at {post.posted_at}</span>
+                                <span>Post content: {post.content}</span>
+                                {
+                                    post.poster_id === data.user.id ?
+                                        (<Form className="p-0" navigate={false} action="/delete-post" method="post"><input name="id" className="hidden" readOnly value={post.id} /><Button className="bg-red-600 hover:bg-red-500">Delete</Button></Form>) :
+                                        null
+                                }
+                            </Card>
+                        );
+                    })
+                }
+            </div>
         </div>
     )
 }
