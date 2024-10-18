146fc953fecd25207f1877e058f92ccd2768da6d
Zachary Lamb <102142742+zlamb1@users.noreply.github.com>
2024-10-18 05:50:22 -0500

    updated user avatar to fallback if image fails to load

--- app/components/UserAvatar.tsx
+++ app/components/UserAvatar.tsx
@@ -1,4 +1,5 @@
 import {AnimatePresence, motion} from "framer-motion";
+import {Avatar, AvatarFallback, AvatarImage} from "@ui/avatar";
 
 export interface UserAvatarProps {
     className?: string,
@@ -7,30 +8,40 @@ export interface UserAvatarProps {
     size?: number | string,
 }
 
-export default function UserAvatar({ className, avatar, userName, size = 25 }: UserAvatarProps) {
+function UserAvatarFallback({ userName }: { userName: string }) {
+    return (
+        <motion.div className="origin-center bg-primary flex justify-center items-center w-full h-full rounded-full"
+                    initial={{scale: 0.5, opacity: 0.25}}
+                    animate={{scale: 1, opacity: 1}}
+                    exit={{scale: 0.5, opacity: 0.25}}
+                    transition={{duration: 0.1}}
+                    key={0}>
+            {userName?.substring(0, 1).toUpperCase()}
+        </motion.div>
+    );
+}
+
+export default function UserAvatar({className, avatar, userName, size = 25}: UserAvatarProps) {
     return (
         <div className={`flex justify-center items-center rounded-full select-none font-medium text-white ` + (className ?? '')}
-             style={{ width: size, height: size }}>
+             style={{width: size, height: size}}>
             <AnimatePresence mode="wait" initial={false}>
-                {
-                    avatar ?
-                        <motion.div className="origin-center"
-                                    initial={{scale: 0.5, opacity: 0.25}}
-                                    animate={{scale: 1, opacity: 1}}
-                                    exit={{scale: 0.5, opacity: 0.25}}
-                                    transition={{duration: 0.1}}
-                                    key={avatar}>
-                            <img src={avatar} alt="Profile avatar"/>
-                        </motion.div> :
-                        <motion.div className="origin-center bg-primary flex justify-center items-center w-full h-full rounded-full"
-                                    initial={{scale: 0.5, opacity: 0.25}}
-                                    animate={{scale: 1, opacity: 1}}
-                                    exit={{scale: 0.5, opacity: 0.25}}
-                                    transition={{duration: 0.1}}
-                                    key={0}>
-                            { userName?.substring(0, 1).toUpperCase() }
-                        </motion.div>
-                }
+                <Avatar className="w-full h-full">
+                    {
+                        avatar ?
+                            <motion.div className="origin-center"
+                                        initial={{scale: 0.5, opacity: 0.25}}
+                                        animate={{scale: 1, opacity: 1}}
+                                        exit={{scale: 0.5, opacity: 0.25}}
+                                        transition={{duration: 0.1}}
+                                        key={avatar}>
+                                <AvatarImage src={avatar} alt={`${userName}'s avatar image`} />
+                                <AvatarFallback>
+                                    <UserAvatarFallback userName={userName} />
+                                </AvatarFallback>
+                            </motion.div> : <UserAvatarFallback userName={userName} />
+                    }
+                </Avatar>
             </AnimatePresence>
         </div>
     );
