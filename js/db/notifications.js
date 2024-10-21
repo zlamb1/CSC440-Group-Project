2f9400b915eefd6ab290b7a80c8c1ed9c19af8c4
Zachary Lamb <102142742+zlamb1@users.noreply.github.com>
2024-10-21 07:14:52 -0500

    made notification count based on viewed notifications

--- js/db/notifications.js
+++ js/db/notifications.js
@@ -1,4 +1,4 @@
-import client, {DBClient, DBError, validateUUID} from "./db.js";
+import client, {DBClient, DBError} from "./db.js";
 
 function formatNotification(row) {
     return {
@@ -7,13 +7,14 @@ function formatNotification(row) {
         type: row.type,
         content: row.content,
         expiresOn: row.expires_on,
+        viewed: row.viewed,
     }
 }
 
 DBClient.prototype.getNotificationCount = async function() {
     await this.throwAuth();
     try {
-        const res = await client.query("SELECT user_id FROM notifications WHERE user_id = $1;", [this.user.id]);
+        const res = await client.query("SELECT user_id FROM notifications WHERE user_id = $1 AND viewed = false;", [this.user.id]);
         return res.rows.length;
     } catch (err) {
         console.error('getNotificationCount: ', err);
@@ -24,7 +25,18 @@ DBClient.prototype.getNotificationCount = async function() {
 DBClient.prototype.getNotifications = async function() {
     await this.throwAuth();
     try {
-        const res = await client.query("SELECT * FROM notifications WHERE user_id = $1;", [this.user.id]);
+        const res = await client.query('SELECT * FROM notifications WHERE user_id = $1;', [this.user.id]);
+        if (res.rows && res.rows.length > 0) {
+            for (const row of res.rows) {
+                if (row.id && !row.viewed) {
+                    try {
+                        client.query('UPDATE notifications SET viewed = true WHERE id = $1', [row.id]);
+                    } catch (err) {
+                        console.error('getNotifications', err);
+                    }
+                }
+            }
+        }
         return res.rows.map(formatNotification);
     } catch (err) {
         console.error('getNotifications: ', err);
