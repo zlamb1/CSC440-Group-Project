feb8e38926c9933d20a6ec37c242bfe4cff86011
Zachary Lamb <102142742+zlamb1@users.noreply.github.com>
2024-10-21 20:37:46 -0500

    added animation to checkboxes

--- app/components/ui/checkbox.tsx
+++ app/components/ui/checkbox.tsx
@@ -1,28 +1,45 @@
 import * as React from "react"
 import * as CheckboxPrimitive from "@radix-ui/react-checkbox"
-import { CheckIcon } from "@radix-ui/react-icons"
 
 import { cn } from "@/lib/utils"
+import {Check} from "lucide-react";
+import {useEffect, useState} from "react";
 
 const Checkbox = React.forwardRef<
   React.ElementRef<typeof CheckboxPrimitive.Root>,
   React.ComponentPropsWithoutRef<typeof CheckboxPrimitive.Root>
->(({ className, ...props }, ref) => (
-  <CheckboxPrimitive.Root
-    ref={ref}
-    className={cn(
-      "grid place-content-center peer h-4 w-4 shrink-0 rounded-sm border border-primary shadow focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground",
-      className
-    )}
-    {...props}
-  >
-    <CheckboxPrimitive.Indicator
-      className={cn("grid place-content-center text-current")}
+>(({ className, ...props }, ref) => {
+  const [isMounted, setIsMounted] = useState(false);
+  useEffect(() => {
+    const id = setTimeout(() => {
+      setIsMounted(true);
+    }, 100);
+    return () => {
+      clearTimeout(id);
+    }
+  }, []);
+  const animationDuration = '0.2s';
+  return (
+    <CheckboxPrimitive.Root
+        ref={ref}
+        className={cn(
+            "grid place-content-center peer h-4 w-4 shrink-0 rounded-sm border border-primary shadow focus-visible:outline-none " +
+            "focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary " +
+            "data-[state=checked]:text-primary-foreground",
+            className
+        )}
+        style={isMounted ? { transition: `background-color ${animationDuration}` } : undefined}
+        {...props}
     >
-      <CheckIcon className="h-4 w-4" />
-    </CheckboxPrimitive.Indicator>
-  </CheckboxPrimitive.Root>
-))
+      <CheckboxPrimitive.Indicator
+          className={cn("grid place-content-center text-current")} forceMount
+      >
+        <Check strokeWidth={3} style={isMounted ? { animationDuration } : undefined} className="z__checkbox-icon__z w-2.5 h-2.5"/>
+      </CheckboxPrimitive.Indicator>
+    </CheckboxPrimitive.Root>
+  )
+});
+
 Checkbox.displayName = CheckboxPrimitive.Root.displayName
 
 export { Checkbox }
