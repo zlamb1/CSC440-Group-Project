5c8fc0fa5b1ab978f2ae5e068c2ce790b5108b08
Zachary Lamb <102142742+zlamb1@users.noreply.github.com>
2024-11-04 19:31:16 -0600

    tweaked route transition

--- app/root.tsx
+++ app/root.tsx
@@ -107,7 +107,7 @@ export default function App() {
                          initial={{opacity: 0}}
                          animate={{opacity: 1}}
                          exit={{opacity: 0}}
-                         transition={{duration: 0.1}}>
+                         transition={{duration: 0.15}}>
                 {outlet}
             </motion.main>
         </AnimatePresence>
