19554a6d2a4fd9d3dd25e2b922932731213ed101
Zachary Lamb <102142742+zlamb1@users.noreply.github.com>
2024-11-10 22:34:49 -0600

    made ProgressCircle fade

--- app/components/ProgressCircle.tsx
+++ app/components/ProgressCircle.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Fade from "./ui/fade";
 
 export interface ProgressCircleProps {
     percentage?: number,
@@ -8,15 +9,17 @@ export interface ProgressCircleProps {
 
 export default function ProgressCircle({ percentage = 0, strokeWidth = 3, hideAtZero = true }: ProgressCircleProps) {
     return (
-        <svg
-            className={"fill-transparent flex w-[24px] h-[24px] overflow-hidden " + (percentage === 0 && hideAtZero ? 'hidden' : '')}>
-            <circle className="stroke-gray-600" style={{strokeWidth}} cx="12" cy="12" r="10" />
-            <circle className="stroke-primary"
-                    style={{strokeWidth, transition: 'stroke-dasharray 0.25s'}}
-                    cx="12" cy="12" r="10"
-                    strokeDasharray={percentage + ',' + (100 - percentage)}
-                    strokeDashoffset="25"
-                    pathLength="100" />
-        </svg>
+        <Fade className="flex items-center" show={!(percentage === 0 && hideAtZero)}>
+            <svg
+                className="fill-transparent flex w-[24px] h-[24px] overflow-hidden">
+                <circle className="stroke-gray-600" style={{strokeWidth}} cx="12" cy="12" r="10"/>
+                <circle className="stroke-primary"
+                        style={{strokeWidth, transition: 'stroke-dasharray 0.25s'}}
+                        cx="12" cy="12" r="10"
+                        strokeDasharray={percentage + ',' + (100 - percentage)}
+                        strokeDashoffset="25"
+                        pathLength="100"/>
+            </svg>
+        </Fade>
     );
 }
--- app/components/ui/fade.tsx
+++ app/components/ui/fade.tsx
@@ -5,6 +5,7 @@ export interface FadeProps {
     children: ReactNode;
     id?: Key | null;
     show?: boolean;
+    className?: string;
     transitionFrom?: number;
     transitionTo?: number;
     duration?: number;
@@ -13,12 +14,13 @@ export interface FadeProps {
     container?: ReactElement;
 }
 
-export default function Fade({ children, id, show = true, transitionFrom = 0, transitionTo = 1, duration = 0.2, initial = true, fallback, container }: FadeProps) {
+export default function Fade({ children, id, show = true, className, transitionFrom = 0, transitionTo = 1, duration = 0.2, initial = true, fallback, container }: FadeProps) {
     return (
         <Transition children={children}
                     id={id}
                     transition={{ property: 'opacity', transitionFrom, transitionTo }}
                     show={show}
+                    className={className}
                     duration={duration}
                     initial={initial}
                     fallback={fallback}
