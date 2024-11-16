25e269d46a04297addfd6628310d67fa37970fcd
Zachary Lamb <102142742+zlamb1@users.noreply.github.com>
2024-11-16 15:02:00 -0600

    updated usePersistedLoaderData

--- app/utils/hooks/usePersistedLoaderData.ts
+++ app/utils/hooks/usePersistedLoaderData.ts
@@ -2,6 +2,9 @@ import {useEffect, useRef} from "react";
 import {useLoaderData} from "@remix-run/react";
 import {loader} from "@/routes/users.$username";
 
+/**
+ * Use this instead of useLoaderData to fix the loader data unmounting during route transitions.
+ */
 export default function usePersistedLoaderData() {
     const lastData = useRef();
     const data = useLoaderData<typeof loader>() || lastData.current;
@@ -12,5 +15,5 @@ export default function usePersistedLoaderData() {
         }
     }, [data]);
 
-    return { data };
+    return data;
 }
