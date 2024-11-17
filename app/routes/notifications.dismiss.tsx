685fbd89efd2cfdb2c723452678ca7be7567bbac
Zachary Lamb <102142742+zlamb1@users.noreply.github.com>
2024-11-16 18:46:54 -0600

    created notifications/dismiss route

---
+++ app/routes/notifications.dismiss.tsx
@@ -0,0 +1,32 @@
+import {ActionFunctionArgs} from "@remix-run/node";
+import UnknownErrorResponse from "@/api/UnknownErrorResponse";
+import UnauthorizedResponse from "@/api/UnauthorizedError";
+import {RequiredFieldResponse} from "@/api/BadRequestResponse";
+import {ExplicitDeleteResponse} from "@/api/DeleteResponse";
+
+export async function action({ context, request }: ActionFunctionArgs) {
+    try {
+        if (!context.user.loggedIn) {
+            return UnauthorizedResponse();
+        }
+
+        const formData = await request.formData();
+        const ids = JSON.parse(formData.getAll('id'));
+
+        if (!ids || !ids.length) {
+            return RequiredFieldResponse('Notification ID');
+        }
+
+        await context.prisma.notification.deleteMany({
+            where: {
+                id: {
+                    in: ids
+                }
+            }
+        });
+
+        return ExplicitDeleteResponse('Notification');
+    } catch (err) {
+        return UnknownErrorResponse(err);
+    }
+}
