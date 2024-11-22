5ed08221b1dfd33c909a57228e5309b0a0e75a44
Zachary Lamb <102142742+zlamb1@users.noreply.github.com>
2024-11-22 03:16:06 -0600

    added search input to SortPanel

--- app/components/SortPanel.tsx
+++ app/components/SortPanel.tsx
@@ -1,18 +1,19 @@
 import {Card} from "@ui/card";
 import {Button} from "@ui/button";
-import {ChevronUp, ChevronDown} from "lucide-react";
+import {ChevronUp, ChevronDown, Search, X} from "lucide-react";
 import {Separator} from "@ui/separator";
 import React, {CSSProperties, useContext, useState} from "react";
 import {cn} from "@/lib/utils";
 import {UserContext} from "@/utils/context/UserContext";
 import {Genre} from "@prisma/client";
 import {formatGenre, GenreThemes} from "@/utils/genre-util";
-import {AnimatePresence, motion} from "framer-motion";
 import Expand from "@ui/expand";
+import {Input} from "@ui/input";
 
 export default function SortPanel({ className, style }: { className?: string, style?: CSSProperties }) {
     const user = useContext(UserContext);
-    const [showContent, setShowContent] = useState(true);
+    const [showContent, setShowContent] = useState<boolean>(true);
+    const [filter, setFilter] = useState<string>('');
 
     const toggleContent = () => {
         setShowContent(!showContent);
@@ -31,6 +32,12 @@ export default function SortPanel({ className, style }: { className?: string, st
             <Separator />
             <Expand className="w-full flex flex-col gap-1 overflow-hidden" show={showContent}>
                 <div className="p-2">
+                    <Input prepend={<Search className="text-muted-foreground" size={16} />}
+                           append={filter && <Button className="rounded-full w-6 h-6" size="icon" variant="ghost" onClick={() => setFilter('')}><X size={16} /></Button>}
+                           value={filter}
+                           onChange={(e) => setFilter(e?.target?.value)}
+                           placeholder="Search Posts"
+                    />
                     <span className="font-bold select-none opacity-70 text-sm mx-1">Genre</span>
                     <div className="w-full flex flex-wrap">
                         {
