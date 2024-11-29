c7f9292050b88fea4fa448729318a7a602cf59c5
Zachary Lamb <102142742+zlamb1@users.noreply.github.com>
2024-11-29 07:34:40 -0600

    moved showInfo button into search field

--- app/components/SearchPanel.tsx
+++ app/components/SearchPanel.tsx
@@ -2,7 +2,7 @@ import {Card} from "@ui/card";
 import {Button} from "@ui/button";
 import {ChevronDown, ChevronUp, Info, Search, X} from "lucide-react";
 import {Separator} from "@ui/separator";
-import React, {CSSProperties, useContext, useEffect, useState} from "react";
+import React, {CSSProperties, Fragment, useContext, useEffect, useState} from "react";
 import {cn} from "@/lib/utils";
 import {UserContext} from "@/utils/context/UserContext";
 import Expand from "@ui/expand";
@@ -13,7 +13,7 @@ import PostSearch from "@components/post/PostSearch";
 export default function SearchPanel({className, style}: { className?: string, style?: CSSProperties }) {
   const user = useContext(UserContext);
   const [showContent, setShowContent] = useState<boolean>(true);
-  const [showInfo, setShowInfo] = useState<boolean>(true);
+  const [showInfo, setShowInfo] = useState<boolean>(false);
   const [filter, setFilter] = useState<string | undefined>('');
   const {_filter} = usePostStore(useShallow((state: any) => ({_filter: state?.filter})));
 
@@ -37,19 +37,31 @@ export default function SearchPanel({className, style}: { className?: string, st
         {showContent ? <ChevronUp size={16}/> : <ChevronDown size={16}/>}
       </Button>
       <Separator/>
-      <Expand className="w-full flex flex-col gap-1 overflow-hidden" show={showContent}>
+      <Expand className="w-full flex flex-col gap-1 overflow-hidden" show={showContent} initial={false}>
         <div className="flex flex-col gap-2 p-2">
           <PostSearch prepend={<Search className="text-muted-foreground" size={18}/>}
-                      append={filter &&
-                        <Button className="text-muted-foreground rounded-full w-7 h-7" size="icon" variant="ghost"
-                                onClick={() => setFilter('')}><X size={18}/></Button>}
+                      append={
+                        <Fragment>
+                          <Button className="w-6 h-6" size="icon" variant="ghost"
+                                  onClick={(evt) => {
+                                    evt.stopPropagation();
+                                    setShowInfo(prev => !prev)
+                                  }}>
+                            <Info size={16}/>
+                          </Button>
+                          {
+                            filter &&
+                            <Button className="text-muted-foreground rounded-full w-7 h-7" size="icon" variant="ghost"
+                                    onClick={() => setFilter('')}>
+                              <X size={18}/>
+                            </Button>
+                          }
+                        </Fragment>
+                      }
                       value={filter}
                       onChange={setFilter}
                       placeholder="Search Posts"
           />
-          <Button className="w-6 h-6" size="icon" variant="ghost" onClick={() => setShowInfo(prev => !prev)}>
-            <Info size={16}/>
-          </Button>
           <Expand show={showInfo} initial={false}>
             <p className="text-sm text-muted-foreground">
               The search box can be used to search the content of a post, or be used to search various attributes such
