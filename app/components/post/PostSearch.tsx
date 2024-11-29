c73f945bd073811c02bc10ffdc59f432f5526875
Zachary Lamb <102142742+zlamb1@users.noreply.github.com>
2024-11-29 16:22:19 -0600

    broke up PostSearch component

--- app/components/post/PostSearch.tsx
+++ app/components/post/PostSearch.tsx
@@ -1,125 +1,189 @@
 import {Input} from "@ui/input";
-import {ComponentProps, Dispatch, Fragment, SetStateAction, useEffect, useRef, useState} from "react";
+import {
+  ComponentProps,
+  Dispatch,
+  forwardRef,
+  Fragment,
+  KeyboardEvent,
+  SetStateAction,
+  useEffect,
+  useRef,
+  useState
+} from "react";
 import {Popover, PopoverContent, PopoverTrigger} from "@ui/popover";
-import useMountedEffect from "@/utils/hooks/useMountedEffect";
 import {formatGenre} from "@/utils/genre-util";
 import {Genre} from "@prisma/client";
+import {cn} from "@/lib/utils";
+
+interface SuggestionItemsProps {
+  value?: string;
+  selectionIndex?: number;
+  onChange?: (value?: string) => void | Dispatch<SetStateAction<string>>;
+  setMaxSelection?: Dispatch<SetStateAction<number>>;
+}
+
+const SuggestionItems = forwardRef<HTMLDivElement, SuggestionItemsProps>(({
+                                                                            value,
+                                                                            selectionIndex,
+                                                                            onChange,
+                                                                            setMaxSelection
+                                                                          }: SuggestionItemsProps, ref) => {
+  const suggestions = [
+    {
+      placeholder: 'genre::<genre>',
+      values: Object.keys(Genre).map(formatGenre)
+    },
+    {
+      placeholder: 'liked::<value>',
+      values: ['true', 'false', 'null']
+    },
+    {
+      placeholder: 'likes::<number>'
+    },
+    {
+      placeholder: 'replies::<number>',
+    }
+  ];
+
+  const attributes = value?.split(' ');
+  const lastAttribute = attributes?.[attributes?.length - 1];
+  const lastValue = lastAttribute?.split('::')?.[1];
+  const regex = /(.*)::/;
+  const isEnteringValue = regex.test(lastAttribute ?? '');
+  const attribute = lastAttribute?.split('::')?.[0];
+  const _suggestions = suggestions.filter(suggestion => suggestion.placeholder.startsWith(attribute ?? ''));
+
+  useEffect(() => {
+    if (_suggestions.length === 1 && isEnteringValue) {
+      const suggestion = _suggestions[0];
+      if (suggestion?.values && suggestion.values.length) {
+        const values = suggestion.values.filter(value => value.startsWith(lastValue ?? ''));
+        if (values.length === 1 && values[0].length === lastValue?.length) {
+          setMaxSelection?.(0);
+        } else {
+          setMaxSelection?.(values.length);
+        }
+      }
+    } else {
+      setMaxSelection?.(_suggestions.length);
+    }
+  }, [value]);
+
+  function onSuggest(suggestion: string) {
+    const index = value?.lastIndexOf?.(suggestion);
+    if (!value || index == null) {
+      return onChange?.(value + suggestion.replace(/<(.*)>/, ''));
+    }
+    onChange?.(value + suggestion.substring(value.length - index));
+  }
+
+  if (_suggestions.length === 1 && isEnteringValue) {
+    const suggestion = _suggestions[0];
+    if (suggestion?.values && suggestion.values.length) {
+      const values = suggestion.values.filter(value => value.startsWith(lastValue ?? ''));
+
+      if (values.length === 1 && values[0].length === lastValue?.length) {
+        // the attribute is complete
+        return null;
+      }
+
+      function onSelectValue(_value: string) {
+        //onChange?.(value + _value.substring(lastValue?.length ?? 0))
+      }
+
+      return (
+        values
+          .map((value, i) => (
+            <div key={value}
+                 className={cn("p-2 text-sm cursor-pointer hover:bg-muted", i === selectionIndex && 'bg-muted')}
+                 onClick={() => onSelectValue(value)}
+                 ref={i === selectionIndex ? ref : undefined}
+            >
+              {value}
+            </div>
+          ))
+      );
+    }
+  }
+
+  return (
+    _suggestions.map((suggestion, i) => (
+      <div key={suggestion.placeholder}
+           className={cn("p-2 text-sm cursor-pointer hover:bg-muted", i === selectionIndex && 'bg-muted')}
+           onClick={() => onSuggest(suggestion.placeholder)}
+           ref={i === selectionIndex ? ref : undefined}
+      >
+        {suggestion.placeholder}
+      </div>
+    ))
+  );
+});
 
 export type PostSearchProps = Omit<ComponentProps<typeof Input>, 'onChange'> & {
-    className?: string;
-    value?: string;
-    onChange?: (value?: string) => void | Dispatch<SetStateAction<string>>;
+  className?: string;
+  value?: string;
+  onChange?: (value?: string) => void | Dispatch<SetStateAction<string>>;
 };
 
-export default function PostSearch({ className, value, onChange, ...props }: PostSearchProps) {
-    const [isOpen, setIsOpen] = useState<boolean>(false);
-    const [isFocused, setIsFocused] = useState<boolean>(false);
-    const ref = useRef<HTMLInputElement>(null);
-    const popoverRef = useRef<HTMLDivElement>(null);
+export default function PostSearch({className, value, onChange, ...props}: PostSearchProps) {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [selectionIndex, setSelectionIndex] = useState<number>(0);
+  const [maxSelection, setMaxSelection] = useState<number>(0);
+  const inputRef = useRef<HTMLInputElement>(null);
+  const popoverRef = useRef<HTMLDivElement>(null);
+  const selectedRef = useRef<HTMLDivElement>(null);
 
-    useEffect(() => {
-        setIsOpen(isFocused);
-    }, [isFocused]);
+  useEffect(() => {
+    setSelectionIndex(0);
+    setMaxSelection(0);
+  }, [value, isOpen]);
 
-    const suggestions = [
-        {
-            placeholder: 'genre::<genre>',
-            values: Object.keys(Genre).map(formatGenre)
-        },
-        {
-            placeholder: 'liked::<value>',
-            values: [ 'true', 'false', 'null' ]
-        },
-        {
-            placeholder: 'likes::<number>'
-        },
-        {
-            placeholder: 'replies::<number>',
-        }
-    ];
-
-    const attributes = value?.split(' ');
-    const lastAttribute = attributes?.[attributes?.length - 1];
-    const lastValue = lastAttribute?.split('::')?.[1];
-    const regex = /(.*)::/;
-    const isEnteringValue = regex.test(lastAttribute ?? '');
-
-    function prepend() {
-        return (
-            <Fragment>
-                {props?.prepend}
-            </Fragment>
-        );
-    }
-    
-    function onSuggest(suggestion: string) {
-        const index = value?.lastIndexOf?.(suggestion);
-        if (!value || index == null) {
-            return onChange?.(value + suggestion.replace(/<(.*)>/, ''));
-        }
-        onChange?.(value + suggestion.substring(value.length - index));
-    }
-
-    function SuggestionItems() {
-        const attribute = lastAttribute?.split('::')?.[0] + '::';
-        const _suggestions = suggestions.filter(suggestion => suggestion.placeholder.startsWith(attribute));
-
-        if (_suggestions.length === 1 && isEnteringValue) {
-            const suggestion = _suggestions[0];
-            if (suggestion?.values && suggestion.values.length) {
-                const values = suggestion.values.filter(value => value.startsWith(lastValue ?? ''));
-
-                if (values.length === 1 && values[0].length === lastValue?.length) {
-                    // the attribute is complete
-                    return null;
-                }
-
-                function onSelectValue(_value: string) {
-                    console.log(_value);
-                    console.log(lastValue);
-                    console.log(value + _value.substring(lastValue?.length ?? 0));
-                    onChange?.(value + _value.substring(lastValue?.length ?? 0))
-                }
-
-                return (
-                    suggestion.values
-                        .filter(value => value.startsWith(lastValue ?? ''))
-                        .map(value => (
-                            <div key={value} className="p-2 text-sm cursor-pointer hover:bg-muted" onClick={() => onSelectValue(value)}>
-                                {value}
-                            </div>
-                        ))
-                );
-            }
-        }
-
-        return (
-            _suggestions.map(suggestion => (
-                <div key={suggestion.placeholder} className="p-2 text-sm cursor-pointer hover:bg-muted" onClick={() => onSuggest(suggestion.placeholder)}>
-                    {suggestion.placeholder}
-                </div>
-            ))
-        );
-    }
-
+  function prepend() {
     return (
-        <Popover open={isOpen}>
-            <PopoverTrigger asChild>
-            <Input {...props}
-                       type="text"
-                       className={className}
-                       prepend={prepend()}
-                       value={value}
-                       onChange={(e) => onChange?.(e?.target?.value)}
-                       onFocus={() => setIsFocused(true)}
-                       onBlur={() => setIsFocused(false)}
-                       placeholder={isFocused ? '' : 'Search Posts'}
-                       ref={ref}
-                />
-            </PopoverTrigger>
-            <PopoverContent ref={popoverRef} className="p-0 flex flex-col" onOpenAutoFocus={(e) => e.preventDefault()}>
-                <SuggestionItems />
-            </PopoverContent>
-        </Popover>
+      <Fragment>
+        {props?.prepend}
+      </Fragment>
     );
+  }
+
+  function onKeyDown(evt: KeyboardEvent) {
+    const key = evt.key.toLowerCase();
+    switch (key) {
+      case 'enter':
+        selectedRef.current?.click?.();
+        break;
+      case 'arrowdown':
+        evt.preventDefault();
+        setSelectionIndex(prev => Math.min(prev + 1, maxSelection - 1));
+        break;
+      case 'arrowup':
+        evt.preventDefault();
+        setSelectionIndex(prev => Math.max(prev - 1, 0));
+        break;
+    }
+  }
+
+  return (
+    <Popover open={isOpen} onOpenChange={setIsOpen}>
+      <PopoverTrigger asChild>
+        <Input {...props}
+               type="text"
+               className={className}
+               prepend={prepend()}
+               value={value}
+               onChange={(e) => onChange?.(e?.target?.value)}
+               onKeyDown={onKeyDown}
+               placeholder={'Search Posts'}
+               ref={inputRef}
+        />
+      </PopoverTrigger>
+      <PopoverContent ref={popoverRef} className="p-0 flex flex-col"
+                      onOpenAutoFocus={(e) => e.preventDefault()}
+      >
+        <SuggestionItems value={value} selectionIndex={selectionIndex} onChange={onChange}
+                         setMaxSelection={setMaxSelection} ref={selectedRef}
+        />
+      </PopoverContent>
+    </Popover>
+  );
 }
