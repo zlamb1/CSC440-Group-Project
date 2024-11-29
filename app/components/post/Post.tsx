f6c0322aa6ef549b21c31a33b3db04c1b8b5168c
Zachary Lamb <102142742+zlamb1@users.noreply.github.com>
2024-11-29 07:18:15 -0600

    updated Post.tsx

--- app/components/post/Post.tsx
+++ app/components/post/Post.tsx
@@ -1,10 +1,7 @@
 import UserAvatar from "@components/user/UserAvatar";
 import {Button} from "@ui/button";
 import React, {useContext, useEffect, useState} from "react";
-import {
-    MessageCircle,
-    Pencil,
-} from "lucide-react";
+import {MessageCircle, Pencil,} from "lucide-react";
 import {Link} from "@remix-run/react";
 import ContextMenu from "@components/post/ContextMenu";
 import PostView from "@components/post/PostView";
@@ -22,112 +19,113 @@ import GenreTags from "@components/post/GenreTags";
 import {PostContext} from "@/utils/context/PostContext";
 
 function Post({className, id, depth = 1, autoReply = true, exitDuration = 0.25}: {
-    className?: string,
-    id: string,
-    depth?: number,
-    autoReply?: boolean,
-    exitDuration?: number,
+  className?: string,
+  id: string,
+  depth?: number,
+  autoReply?: boolean,
+  exitDuration?: number,
 }) {
-    const viewer = useContext(UserContext);
-    const [isEditing, setEditing] = useState<boolean>(false);
-    const [isReplying, setIsReplying] = useState<boolean>(!!viewer?.loggedIn && autoReply);
-    const [showReplies, setShowReplies] = useState<boolean>(depth > 0);
-    const [formattedTime, setFormattedTime] = useState<string>('');
+  const viewer = useContext(UserContext);
+  const [isEditing, setEditing] = useState<boolean>(false);
+  const [isReplying, setIsReplying] = useState<boolean>(!!viewer?.loggedIn && autoReply);
+  const [showReplies, setShowReplies] = useState<boolean>(depth > 0);
+  const [formattedTime, setFormattedTime] = useState<string>('');
 
-    const { post } = usePostStore(useShallow((state: any) => ({ post: state[id] })));
+  const {post} = usePostStore(useShallow((state: any) => ({post: state[id]})));
 
-    useEffect(() => {
-        const date = new Date(post?.postedAt);
-        const diff = new Date().getTime() - date.getTime();
+  useEffect(() => {
+    const date = new Date(post?.postedAt);
+    const diff = new Date().getTime() - date.getTime();
 
-        if ((post?.postedAt && !formattedTime) || diff < 1000 * 60) {
-            const id = setInterval(() => setFormattedTime(formatPastDate(post?.postedAt)));
-            return () => clearInterval(id);
-        }
-    });
-
-    if (!post) {
-        return (
-            <Card className="my-3 p-3">
-                <span>Post not found</span>
-            </Card>
-        )
+    if ((post?.postedAt && !formattedTime) || diff < 1000 * 60) {
+      const id = setInterval(() => setFormattedTime(formatPastDate(post?.postedAt)));
+      return () => clearInterval(id);
     }
+  });
 
-    function getReplyTree() {
-        return (
-            <AnimatePresence>
-                {
-                    post?.replies?.map((reply: string) => <Post key={reply} id={reply} depth={depth - 1} autoReply={false} />)
-                }
-            </AnimatePresence>
-        )
-    }
-
+  if (!post) {
     return (
-        <PostContext.Provider value={post}>
-            <motion.div initial={{opacity: 0}}
-                        animate={{opacity: 1, height: 'auto'}}
-                        exit={{opacity: 0, height: 0}}
-                        transition={{duration: exitDuration}}
-                        className="flex overflow-y-hidden"
-                        key={post.id}
-            >
-                <div className={cn("flex flex-col w-full", className)}>
-                    <div className="flex justify-between items-center gap-3 flex-grow-0 overflow-x-hidden">
-                        <div className="flex gap-1 items-center flex-grow-0 flex-wrap">
-                            <UserHoverCard user={post.user}>
-                                <Link to={`/users/${post.user?.userName}`} className="font-bold flex flex-row gap-3 flex-shrink-0">
-                                    <UserAvatar avatar={post.user?.avatarPath} userName={post.user?.userName}/>
-                                    <div className="flex items-center gap-1">
-                                        {post.user?.userName}
-                                        <span className="text-sm text-gray-400"
-                                              suppressHydrationWarning>• {formattedTime}</span>
-                                    </div>
-                                </Link>
-                            </UserHoverCard>
-                            <GenreTags genres={post.genres} editable={post?.userId === viewer?.id} />
-                        </div>
-                        <div className="h-full">
-                            <ContextMenu post={post} exitDuration={exitDuration} onEdit={() => setEditing(true)}/>
-                        </div>
-                    </div>
-                    <div className="ml-10 mt-1 flex flex-col gap-3">
-                        <div className="flex flex-col gap-1">
-                            <div className="flex flex-col">
-                                <PostView post={post} isEditing={isEditing} onIsEditingChange={(_isEditing: boolean) => setEditing(_isEditing)} />
-                            </div>
-                            <div className="flex gap-2">
-                                <LikePanel post={post}/>
-                                <div className="flex items-center gap-1 rounded-full bg-gray-100 dark:bg-gray-900">
-                                    <Button containerClass="flex"
-                                            className="h-[25px] flex gap-1 items-center rounded-full"
-                                            size="icon"
-                                            variant={isReplying ? undefined : 'ghost'}
-                                            disabled={!viewer?.loggedIn}
-                                            onClick={() => setIsReplying(!isReplying)}>
-                                        <Pencil size={16}/>
-                                    </Button>
-                                </div>
-                                <div className="flex items-center gap-1 rounded-full bg-gray-100 dark:bg-gray-900">
-                                    <Button containerClass="flex"
-                                            className="h-[25px] flex gap-1 items-center rounded-full"
-                                            size="icon"
-                                            variant="ghost"
-                                            onClick={() => setShowReplies(!showReplies)}>
-                                        <MessageCircle size={16}/>
-                                        <span className="text-bold">{post.replyCount}</span>
-                                    </Button>
-                                </div>
-                            </div>
-                        </div>
-                        {viewer?.loggedIn && <ReplyEditor post={post} isReplying={isReplying}/>}
-                        {getReplyTree()}
-                    </div>
+      <Card className="my-3 p-3">
+        <span>Post not found</span>
+      </Card>
+    )
+  }
+
+  function getReplyTree() {
+    return (
+      <AnimatePresence>
+        {
+          post?.replies?.map((reply: string) => <Post key={reply} id={reply} depth={depth - 1} autoReply={false}/>)
+        }
+      </AnimatePresence>
+    )
+  }
+
+  return (
+    <PostContext.Provider value={post}>
+      <motion.div initial={{opacity: 0}}
+                  animate={{opacity: 1, height: 'auto'}}
+                  exit={{opacity: 0, height: 0}}
+                  transition={{duration: exitDuration}}
+                  className="flex overflow-y-hidden"
+                  key={post.id}
+      >
+        <div className={cn("flex flex-col w-full", className)}>
+          <div className="flex justify-between items-center gap-3 flex-grow-0 overflow-x-hidden">
+            <div className="flex gap-1 items-center flex-grow-0 flex-wrap">
+              <UserHoverCard user={post.user}>
+                <Link to={`/users/${post.user?.userName}`} className="font-bold flex flex-row gap-3 flex-shrink-0">
+                  <UserAvatar avatar={post.user?.avatarPath} userName={post.user?.userName}/>
+                  <div className="flex items-center gap-1">
+                    {post.user?.userName}
+                    <span className="text-sm text-gray-400"
+                          suppressHydrationWarning>• {formattedTime}</span>
+                  </div>
+                </Link>
+              </UserHoverCard>
+              <GenreTags genres={post.genres} editable={post?.userId === viewer?.id}/>
+            </div>
+            <div className="h-full">
+              <ContextMenu post={post} exitDuration={exitDuration} onEdit={() => setEditing(true)}/>
+            </div>
+          </div>
+          <div className="ml-10 mt-1 flex flex-col gap-3">
+            <div className="flex flex-col gap-1">
+              <div className="flex flex-col">
+                <PostView post={post} isEditing={isEditing}
+                          onIsEditingChange={(_isEditing: boolean) => setEditing(_isEditing)}/>
+              </div>
+              <div className="flex gap-2">
+                <LikePanel post={post}/>
+                <div className="flex items-center gap-1 rounded-full bg-gray-100 dark:bg-gray-900">
+                  <Button containerClass="flex"
+                          className="h-[25px] flex gap-1 items-center rounded-full"
+                          size="icon"
+                          variant={isReplying ? undefined : 'ghost'}
+                          disabled={!viewer?.loggedIn}
+                          onClick={() => setIsReplying(!isReplying)}>
+                    <Pencil size={16}/>
+                  </Button>
                 </div>
-            </motion.div>
-        </PostContext.Provider>
-    );
+                <div className="flex items-center gap-1 rounded-full bg-gray-100 dark:bg-gray-900">
+                  <Button containerClass="flex"
+                          className="h-[25px] flex gap-1 items-center rounded-full"
+                          size="icon"
+                          variant="ghost"
+                          onClick={() => setShowReplies(prev => !prev)}>
+                    <MessageCircle size={16}/>
+                    <span className="text-bold">{post.replyCount}</span>
+                  </Button>
+                </div>
+              </div>
+            </div>
+            {viewer?.loggedIn && <ReplyEditor post={post} isReplying={isReplying}/>}
+            {getReplyTree()}
+          </div>
+        </div>
+      </motion.div>
+    </PostContext.Provider>
+  );
 }
 
 export default React.memo(Post);
