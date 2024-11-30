30e9a8cabb68acf4b9085d84910004079914ce0d
Zachary Lamb <102142742+zlamb1@users.noreply.github.com>
2024-11-30 08:14:55 -0600

    added conflict http status to enum

--- app/api/EndpointResponse.ts
+++ app/api/EndpointResponse.ts
@@ -1,19 +1,20 @@
 import {json} from "@remix-run/node";
 
 export const ResponseType = {
-    Success: 200,
-    Created: 201,
-    BadRequest: 400,
-    Unauthorized: 401,
-    Forbidden: 403,
-    ResourceNotFound: 404,
-    Unknown: 500,
+  Success: 200,
+  Created: 201,
+  BadRequest: 400,
+  Unauthorized: 401,
+  Forbidden: 403,
+  ResourceNotFound: 404,
+  Conflict: 409,
+  Unknown: 500,
 }
 
 export default function EndpointResponse(message: string | object, statusCode?: number) {
-    if (typeof message === "string") {
-        return json({ error: message }, { status: statusCode ?? ResponseType.Success })
-    } else {
-        return json(message, { status: statusCode ?? ResponseType.Success });
-    }
+  if (typeof message === "string") {
+    return json({error: message}, {status: statusCode ?? ResponseType.Success})
+  } else {
+    return json(message, {status: statusCode ?? ResponseType.Success});
+  }
 }
