89ab25186afa27865ce81b1b88fce0058db6c59c
Zachary Lamb <102142742+zlamb1@users.noreply.github.com>
2024-12-01 02:10:04 -0600

    formatted posts.public.tsx

--- app/routes/posts.public.tsx
+++ app/routes/posts.public.tsx
@@ -4,17 +4,17 @@ import {getPublicPosts} from "@prisma/client/sql";
 import EndpointResponse from "@/api/EndpointResponse";
 
 export async function fetchPublicPosts(prisma: any, userId: string, cursor?: string | null, limit?: string | null) {
-    return await prisma.$queryRawTyped(getPublicPosts(userId, cursor ? new Date(cursor) : new Date(), limit ? Number(limit) : 5));
+  return await prisma.$queryRawTyped(getPublicPosts(userId, cursor ? new Date(cursor) : new Date(), limit ? Number(limit) : 5));
 }
 
-export async function loader({ context, request }: LoaderFunctionArgs) {
-    try {
-        const url = new URL(request.url);
-        const cursor = url.searchParams?.get("cursor");
-        const limit = url.searchParams?.get("limit");
-        const posts = await fetchPublicPosts(context.prisma, context.user.id, cursor, limit);
-        return EndpointResponse({ posts });
-    } catch (err) {
-        return UnknownErrorResponse(err);
-    }
+export async function loader({context, request}: LoaderFunctionArgs) {
+  try {
+    const url = new URL(request.url);
+    const cursor = url.searchParams?.get("cursor");
+    const limit = url.searchParams?.get("limit");
+    const posts = await fetchPublicPosts(context.prisma, context.user.id, cursor, limit);
+    return EndpointResponse({posts});
+  } catch (err) {
+    return UnknownErrorResponse(err);
+  }
 }
